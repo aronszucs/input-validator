b24bb175e49ee4cc3de394390b96ded85cf8e921
aronszucs <szucsaron@hotmail.com>
2019-07-23 12:14:19 +0200

    basic regex validator complete

--- InputValidator/InputValidatorForm.cs
+++ InputValidator/InputValidatorForm.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Text.RegularExpressions;
 
 namespace InputValidator
 {
@@ -15,6 +16,25 @@ namespace InputValidator
         public InputValidatorForm()
         {
             InitializeComponent();
+        }
+
+        private void SaveButton_Click(object sender, EventArgs e)
+        {
+            string nameRegex = @"^[a-zA-Z]*$";
+            string phoneRegex = @"^(\d{3})\-(\d{3})\-(\d{4})$";
+            string emailRegex = @"^\w+@\w+\.[a-z]{2,3}$";
+            if (!Regex.IsMatch(nameTextBox.Text, nameRegex))
+            {
+                MessageBox.Show("The name is invalid (only alphabetical characters are allowed)");
+            }
+            if (!Regex.IsMatch(phoneTextBox.Text, phoneRegex))
+            {
+                MessageBox.Show("The phone number is not a valid US phone number.");
+            }
+            if (!Regex.IsMatch(emailTextBox.Text, emailRegex))
+            {
+                MessageBox.Show("The e-mail address is not valid.");
+            }
         }
     }
 }
